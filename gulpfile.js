ed5e2f5c60ddf7ff44d6507a83c2e333bd8525c8
Michal Piotrowski <michalpiotrowski86@gmail.com>
2017-06-28 20:29:58 +0200

    Zmodyfikowałem pliki projektu

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-//var jshint = require('gulp-jshint');
+var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
@@ -18,7 +18,13 @@ gulp.task("sass",function(){
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest("css"))
 });
+
+gulp.task("lint",function(){
+	return gulp.src("js/app.js")
+	.pipe(jshint())
+	.pipe(jshint.reporter())
+});
 	
 gulp.task("watch",function(){
-	gulp.watch("scss/*.scss",["sass"]);
+	gulp.watch("scss/*.scss",["sass","lint"]);
 })
